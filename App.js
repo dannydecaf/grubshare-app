61cb1c65d39a5cf84a6390da73ee1e47d68290c3
Dan Coffey <dcoffey90@gmail.com>
2023-07-30 14:48:09 +0100

    Removed Navigation folder & put contents in app.js

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-import AppNavigation from './src/navigation/appNavigation';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen, RegisterScreen, LoginScreen } from './src/screens';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <AppNavigation />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+        <Stack.Screen name="Register" options={{ headerShown: false }} component={RegisterScreen} />
+        <Stack.Screen name="Login" options={{ headerShown: false }} component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
