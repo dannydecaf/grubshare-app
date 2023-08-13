ede3fa3511555a9a5811908d828d7b7036325bc9
Dan Coffey <dcoffey90@gmail.com>
2023-08-13 13:31:53 +0100

    Added Firebase

---
+++ firebaseconfig.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyCwaW2A-T8tDU9csin_8YmYRdo7tYH8ElM",
+    authDomain: "grubshare-98b87.firebaseapp.com",
+    projectId: "grubshare-98b87",
+    storageBucket: "grubshare-98b87.appspot.com",
+    messagingSenderId: "858096806566",
+    appId: "1:858096806566:web:0207efb3ca3feefd9c2267"
+  };
